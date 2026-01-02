2f53a505ac0d91d89a4e54c3677bcbe5e714daff
kelian343 <kelian3427@gmail.com>
2026-01-02 22:45:28 +0800

    Initial Commit

---
+++ Game/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Game
+//
+//  Created by 伈佊 on 1/2/26.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    @IBOutlet var window: NSWindow!
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        
+    }
+    
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+    
+}
---
+++ GameTests/GameTests.swift
@@ -0,0 +1,17 @@
+//
+//  GameTests.swift
+//  GameTests
+//
+//  Created by 伈佊 on 1/2/26.
+//
+
+import Testing
+@testable import Game
+
+struct GameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
